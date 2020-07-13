71d2bd26948125087695a1cd6d237546394fd7c2
DisDylan <dylan.poinsu@gmail.com>
2020-07-13 10:30:26 +0200

    recursive fibonacci

--- ft_fibonacci.c
+++ ft_fibonacci.c
@@ -1,32 +1,18 @@
 int	ft_fibonacci(int index)
 {
-	int a;
-	int b;
-	int new;
-	int i;
-
-	a = 0;
-	b = 1;
-	i = 2;
 	if (index < 0)
 		return (-1);
 	if (index == 0)
 		return (0);
 	if (index == 1)
 		return (1);
-	while (i <= index)
-	{
-		new = a + b;
-		a = b;
-		b = new;
-		i++;
-	}
-	return (new);
+	else
+		return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
 }
 
 #include <stdio.h>
 
 int	main(void)
 {
-	printf("%d", ft_fibonacci(35));
+	printf("%d", ft_fibonacci(8));
 }
