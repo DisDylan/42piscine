b134c5155495deeda5e48a0104ca5f7cc8f3b56c
DisDylan <dylan.poinsu@gmail.com>
2020-07-21 11:28:57 +0200

    push pseudo code split

---
+++ C09/ex02/ft_split.c
@@ -0,0 +1,25 @@
+char	*cut_str(char *str, char *charset, char *final)
+	int i;
+	int j;
+	int k;
+	
+	i = 0;
+	j = 0;
+	k = 0;
+	tant que str != 0:
+		si str i == charset j
+			tant que j != strlen(charset):
+				j++;
+			si j == strlen(charset)
+				si tableau = malloc(sizeof(str) * k);
+					str -> tableau
+				
+
+char	**ft_split(char *str, char *charset)
+{
+	char *final;
+
+	while final = &function(str, charset)
+		final[i] = function
+	final = &function(str charset);
+}
