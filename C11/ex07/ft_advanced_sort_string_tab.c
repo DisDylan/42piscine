5ccf1a3df238e1967fa355a3d4a1d4c82b796e0b
Dylan POINSU <dpoinsu@e2r10p12.42.fr>
2020-07-22 18:27:54 +0200

    last push

--- C11/ex07/ft_advanced_sort_string_tab.c
+++ C11/ex07/ft_advanced_sort_string_tab.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*   ft_advanced_sort_string_tab.c                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dpoinsu <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/22 14:58:23 by dpoinsu           #+#    #+#             */
-/*   Updated: 2020/07/22 17:04:22 by dpoinsu          ###   ########.fr       */
+/*   Created: 2020/07/22 17:06:15 by dpoinsu           #+#    #+#             */
+/*   Updated: 2020/07/22 17:17:39 by dpoinsu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,14 @@ int		ft_strlen(char *str)
 	return (i);
 }
 
-void	ft_swap(char *a, char *b)
-{
-	int swap;
-
-	swap = *a;
-	*a = *b;
-	*b = swap;
-}
-
-int		ft_strcmp(char *s1, char *s2)
-{
-	int i;
-
-	i = 0;
-	while (s1[i] != '\0' || s2[i] != '\0')
-	{
-		if (s1[i] < s2[i])
-			return (1);
-	}
-	return (0);
-}
-
-void	ft_sort_string_tab(char **tab)
+void	ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *))
 {
 	int i;
 
 	i = 0;
 	while (i < ft_strlen(*tab) - 1)
 	{
-		if (ft_strcmp(tab[i], tab[i + 1]))
-		{
-			ft_swap(tab[i], tab[i + 1]);
-			i = 0;
-		}
-		else
-			i++;
+		(*cmp)(tab[i], tab[i + 1]);
+		i++;
 	}
 }
